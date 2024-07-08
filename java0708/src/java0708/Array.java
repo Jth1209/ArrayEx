ccefdfa83ee72a82b23488cc6f62ca2253c37d80
jth1209 <xkgshand@naver.com>
2024-07-08 19:00:22 +0900

    first commit

---
+++ java0708/src/java0708/Array.java
@@ -0,0 +1,20 @@
+package java0708;
+
+public class Array {
+	public static void main(String[] args) {
+		int[] score = {83, 90 ,87};
+		
+		System.out.println("score[0]:"+ score[0]);
+		System.out.println("score[1]:"+ score[1]);
+		System.out.println("scores[2]:"+ score[2]);
+		
+		int sum = 0;
+		for(int i = 0; i < score.length; i++) {
+			sum = sum + score[i];
+		}
+		System.out.println("총합:" + sum);
+		double avg = (double) sum / score.length;
+		System.out.println("평균:" + avg);
+	}
+
+}
